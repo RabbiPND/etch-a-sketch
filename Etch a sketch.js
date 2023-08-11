bee2cba764a11803a64c6415f4a4bd7f6c38dbb0
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-08-11 07:22:54 +0200

    unfinished project
    unfinished project

--- Etch a sketch.js
+++ Etch a sketch.js
@@ -1,14 +1,36 @@
-const resetBtn = document.querySelector('#reset-btn')
-const gridContainer = document.querySelector('#container')
+
 const newGrid = document.querySelector('#new-grid')
+newGrid.addEventListener('click', () => {
+const number = parseInt(prompt('Enter a grid size')) 
+ 
+    if(!isNaN(number)) {
+     console.log(`You entered ${number}`);
+     if(number <= 16) {
+        createGrid(number)
+     } else {
+        alert('Please enter a grid size that is less or equal to 16')
+    } 
+    }
+ })
+
+ const resetBtn = document.querySelector('#reset-btn')
+function clearGrid() {
+    const cells = gridContainer.querySelectorAll('.grid-item');
+    cells.forEach(gridItem => {
+        gridItem.style.backgroundColor = 'white';
+    });
+
+}
+resetBtn.addEventListener('click', clearGrid)
 
 
+const gridContainer = document.querySelector('#container')
+function createGrid(size) {
+     gridContainer.innerHTML = ''; // Clear existing grid
+     
 
-function createGrid(rows, cols) {
-    // gridContainer.innerHTML = ''; // Clear existing grid
-
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             const gridItem = document.createElement("div");
 
             gridItem.classList.add("grid-item");
@@ -22,31 +44,10 @@ function createGrid(rows, cols) {
 }
 
 // Call the function to create a 16x16 grid
-createGrid(16, 16)
-
-function clearGrid() {
-    const cells = gridContainer.querySelectorAll('.grid-item');
-    cells.forEach(gridItem => {
-        gridItem.style.backgroundColor = 'white';
-    });
-
-}
+createGrid(size)
 
 
-resetBtn.addEventListener('click', clearGrid)
 
-newGrid.addEventListener('click', () => {
-   const number = parseInt(prompt('Enter a grid size')) 
-
-   if(!isNaN(number)) {
-    console.log(`You entered ${number}`);
-    createGrid(number)
-
-   } else {
-    alert('Please enter a grid size that is less or equal to 100') // insert text NB
-   }
-
-})
 
 
 
